e67377b99db8a9a33ab98d4cbaeadcf24c8f5dbc
vanhuy2709 <hvhuy279@gmail.com>
2023-10-12 18:15:18 +0700

    Build Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
+import logo from "../assets/images/youtube-dark-icon.png";
+import "../styles/header.scss";
+
 function Header() {
-    
-    return (
-        <div className="header">
-            <img src='/public/logo192.png' alt="logo-header" />
-            <h4>ANTHONY WIKTOR</h4>
-        </div>
-    )
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img src={logo} alt="logo-header" className="header__logo--img" />
+        <h4 className="header__logo--title">ANTHONY WIKTOR</h4>
+      </div>
+
+      <div className="header__route">
+        <a href="https://facebook.com" className="header__route--link">
+          ABOUT
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,9 @@
-import './App.scss'
-
-import Header from '../components/Header';
+import "./App.scss";
+import Header from "../components/Header";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
     </div>
   );
