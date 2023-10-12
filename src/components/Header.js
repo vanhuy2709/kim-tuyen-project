dce13eafca4d2094eb28bbf7f410c812f0e9c282
vanhuy2709 <hvhuy279@gmail.com>
2023-10-12 17:20:15 +0700

    Build Structure React

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    
+    return (
+        <div className="header">
+            <img src='/public/logo192.png' alt="logo-header" />
+            <h4>ANTHONY WIKTOR</h4>
+        </div>
+    )
+}
+
+export default Header;
